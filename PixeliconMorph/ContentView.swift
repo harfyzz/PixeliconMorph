b3b321cbeef473b688ccdbcb93dee3f19a3fca19
Afeez <harfyzz@gmail.com>
2026-07-07 14:08:58 +0100

    Initial Commit

---
+++ PixeliconMorph/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PixeliconMorph
+//
+//  Created by Afeez Yunus on 07/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PixeliconMorph/PixeliconMorphApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixeliconMorphApp.swift
+//  PixeliconMorph
+//
+//  Created by Afeez Yunus on 07/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct PixeliconMorphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
